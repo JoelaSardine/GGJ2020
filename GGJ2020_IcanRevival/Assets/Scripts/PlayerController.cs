08029d21799bb500f1cbea1c34b923792dbb08df
Joe la Sardine <thibaut.poittevin@gmail.com>
2020-01-31 23:15:36 +0100

    Work on player controller

---
+++ GGJ2020_IcanRevival/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayerController : MonoBehaviour
+{
+    private const string INPUT_AXIS_HORIZONTAL = "Horizontal";
+    private const string INPUT_AXIS_VERTICAL = "Vertical";
+    private const string INTERACTION_COLLIDER = "InteractionCollider";
+
+
+    public int playerNum;
+
+
+    private bool _movementEnabled = true;
+    public bool movementEnabled {
+        get { return _movementEnabled; }
+        set {
+            velocity = Vector2.zero;
+            animator.SetBool("Moving", false);
+            _movementEnabled = value;
+        }
+    }
+
+    public float speed = 5.0f;
+    public Vector2 velocity = Vector2.zero;
+
+    public Vector2 movingDirection = Vector2.zero;
+    public Vector2 lookingDirection = Vector2.zero;
+
+    private new Rigidbody2D rigidbody;
+    private Animator animator;
+    private float interactionRange;
+    private Collider2D interactionCollider;
+
+    public InputDevice device;
+    public string deviceMeta;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        interactionCollider = transform.Find(INTERACTION_COLLIDER).GetComponent<Collider2D>();
+        interactionRange = interactionCollider.transform.localPosition.magnitude;
+    }
+
+    private void Update()
+    {
+        //InputDevice inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
+
+        if (device == null)
+        {
+            // No controller for this player number
+        }
+        else
+        {
+            ManageInput();
+        }
+    }
+
+    public void ManageInput()
+    {
+        if (movementEnabled)
+        {
+            Vector2 movingInput = device.LeftStick;
+            float len = movingInput.sqrMagnitude;
+            if (len > 1)
+            {
+                movingInput.Normalize();
+            }
+
+            Vector2 lookingInput = device.RightStick;
+
+            if (len < float.Epsilon)
+            {
+                animator.SetBool("Moving", false);
+            }
+            else
+            {
+                animator.SetBool("Moving", true);
+
+                if (lookingInput.sqrMagnitude < float.Epsilon)
+                {
+                    lookingInput = movingInput;
+                }
+            }
+
+            animator.SetFloat("Horizontal", lookingInput.x);
+            animator.SetFloat("Vertical", lookingInput.y);
+
+            if (lookingInput.sqrMagnitude > float.Epsilon)
+            {
+                lookingDirection = lookingInput.normalized;
+            }
+            movingDirection = movingInput;
+
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        rigidbody.velocity = movingDirection * speed;
+        interactionCollider.transform.localPosition = lookingDirection * interactionRange;
+    }
+}
---
+++ GGJ2020_IcanRevival/Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayersManager : MonoBehaviour
+{
+    public enum State { Home, InGame, Rebind }
+    public State state = State.Home;
+
+    public GameObject playerPrefab;
+
+    public List<PlayerController> players;
+
+    public int deviceCount = 1;
+    public bool isBindingInputs = false;
+
+    private List<PlayerController> playersToRemove = new List<PlayerController>();
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Home:
+                UpdateHome();
+                break;
+            case State.InGame:
+                UpdateInGame();
+                break;
+            case State.Rebind:
+                UpdateRebind();
+                break;
+        }
+
+        if (deviceCount != InputManager.Devices.Count)
+        {
+            deviceCount = InputManager.Devices.Count;
+        }
+    }
+
+    private void UpdateHome()
+    {
+        foreach (var device in InputManager.Devices)
+        {
+            bool alreadyBound = false;
+
+            foreach (var player in players)
+            {
+                if (device.Meta == player.deviceMeta)
+                {
+                    alreadyBound = true;
+
+                    if (device.Action2)
+                    {
+                        playersToRemove.Add(player);
+                    }
+                    break;
+                }
+            }
+            if (alreadyBound)
+            {
+                continue;
+            }
+
+            if (device.Action1)
+            {
+                CreateNewPlayer(device);
+            }
+        }
+
+        foreach (var player in playersToRemove)
+        {
+            RemovePlayer(player);
+        }
+        playersToRemove.Clear();
+    }
+
+    private void UpdateInGame()
+    { }
+
+    private void UpdateRebind()
+    { }
+
+    private void CreateNewPlayer(InputDevice device)
+    {
+        GameObject go = Instantiate(playerPrefab);
+        PlayerController pc = go.GetComponent<PlayerController>();
+        pc.device = device;
+        pc.deviceMeta = device.Meta;
+
+        players.Add(pc);
+    }
+
+    private void RemovePlayer(PlayerController player)
+    {
+        Destroy(player.gameObject);
+        players.Remove(player);
+    }
+}

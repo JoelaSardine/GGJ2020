0ab1a58a54a0599f00907fe836b7330b117f1b05
franck <franckmorgand@hotmail.fr>
2020-02-02 13:47:34 +0100

    Added breakable machine

--- GGJ2020_IcanRevival/Assets/Scripts/Machine.cs
+++ GGJ2020_IcanRevival/Assets/Scripts/Machine.cs
@@ -1,39 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Machine : Interactable
 {
+    public MiniGameType repairGame;
+    public float repairArgument;
+    public UnityEvent OnRepairFinished;
+
     public ItemType ItemRequired;
     public bool broken;
 
+    public virtual void Start()
+    {
+        OnRepairFinished.AddListener(RepairEvent);
+        StartCoroutine(CheckForBroken());
+    }
+
+
     public override void InteractWithItem(Interactable itemHolded)
     {
-        Item item = itemHolded as Item;
-
-        if (item != null)
-        {
-            if (ItemRequired == item.type)
-            {
-                Debug.Log("Type equal = true");
-            }
-            else
-            {
-                Debug.Log("Type equal = false");
-            }
-        }
-        else if (ItemRequired == ItemType.None)
-        {
-            Debug.Log("Type equal = true");
-        }
-        else
-        {
-            Debug.Log("Type equal = false");
-        }
+        
     }
 
     public override void Interact(PlayerController player)
     {
 
+    }
+
+    public void RepairEvent()
+    {
+        broken = false;
+
+        ChangeName(this.gameObject.name);
+    }
+
+    IEnumerator CheckForBroken()
+    {
+        while(true)
+        {
+            if (Random.Range(0, 100) > 97)
+            {
+                broken = true;
+                ChangeName("Broken");
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+        
     }
 }
